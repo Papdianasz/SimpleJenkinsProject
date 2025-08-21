e0a51f44b11ae0bbdc1915a2381b2273fc290dc8
Papdianasz <papdianasz@gmail.com>
2025-08-21 13:12:47 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(200);
+
+        }
+
+        }
+    }
